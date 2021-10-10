be059cb5eb2b7e7e214a49395f1c91ebb9e1e714
Oskar Korczak <oskar.korczak@protonmail.com>
2021-10-10 16:01:42 +0200

    kod z drugich cwiczen

---
+++ sem1/PPJ/zjazd1/Program1.java
@@ -0,0 +1,14 @@
+public class Program1 {
+	public static void main (String[] args) {
+		System.out.println("HELLO");
+		System.out.println(true);
+		System.out.println(false);
+		System.out.println(20);
+		System.out.println(0b10100);
+		System.out.println(0x14);
+		System.out.println(024);
+		System.out.println(21.37f);
+		System.out.println(14.88d);
+		System.out.println('\u0119');
+	}
+}
