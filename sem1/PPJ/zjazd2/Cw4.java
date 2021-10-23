4ff37ebb64c71d0f3126d4887ace785dd303103f
Oskar Korczak <oskarissimus@gmail.com>
2021-10-23 21:11:16 +0200

    hehe

--- sem1/PPJ/zjazd2/Cw4.java
+++ sem1/PPJ/zjazd2/Cw4.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main (String[] args) {
         // 0x24229 - numer studenta w hexie
-        System.out.println(0b1001_0000_1000_1010_01); //numer studenta w hexie przekonwertowany na bin
+        System.out.println(0b0010_0100_0010_0010_1001); //numer studenta w hexie przekonwertowany na bin
     }
 }
