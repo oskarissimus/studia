82d3d03670a0e62c67100fc920f0f0e9d9f49660
Oskar Korczak <oskarissimus@gmail.com>
2021-10-23 21:39:22 +0200

    sd

--- sem1/PPJ/zjazd2/Cw7.java
+++ sem1/PPJ/zjazd2/Cw7.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main (String[] args) {
-        for (int i=0 ; i < 2*2*2*2*2 ; i++) {
-            String n_bin_str = String.format("%5s", Integer.toBinaryString(i)).replace(' ', '0');
-            System.out.println(n_bin_str);
-        }
+        // skopiowanie, lustrzane odbicie i dodanie bitu z przodu
     }
 }
