7de66b21b1e46b15631c0d6017ccc086e99d6137
Oskar Korczak <oskarissimus@gmail.com>
2021-10-23 21:03:44 +0200

    cw5

---
+++ sem1/PPJ/zjazd2/Cw5.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main (String[] args) {
+        System.out.println(isPrime(13));
+        System.out.println(isPrime(15));
+        System.out.println(isPrime(17));
+    }
+}
