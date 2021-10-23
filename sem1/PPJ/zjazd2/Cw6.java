7f60b443e68118aee5910fdf271028bc18c80a12
Oskar Korczak <oskarissimus@gmail.com>
2021-10-23 21:32:40 +0200

    xdd

---
+++ sem1/PPJ/zjazd2/Cw6.java
@@ -0,0 +1,8 @@
+public class Cw6 {
+    public static void main (String[] args) {
+        int x = 2 * 5 + 3 * 4 - 8;
+        System.out.println(x);
+        x = 2 * (((5 + 3) * 4) - 8);
+        System.out.println(x);
+    }
+}
