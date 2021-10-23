590df2c92e404f9efbcf40be7aea49c8fd84c7bc
Oskar Korczak <oskarissimus@gmail.com>
2021-10-23 20:34:37 +0200

    cw1

---
+++ sem1/PPJ/zjazd2/Cw1.java
@@ -0,0 +1,29 @@
+public class Cw1 {
+
+    public static void main(String[] args) {
+        int a = 21, b=37;
+        double x = 13.37, y = 14.88;
+        char n = 'n', m = 'z';
+
+        System.out.println("działania na liczbach całkowitych");
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+        System.out.println("działania na liczbach rzeczywistych");
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        System.out.println(x%y);
+
+        System.out.println("działania na znakach");
+        System.out.println(n+m);
+        System.out.println(n-m);
+        System.out.println(n*m);
+        System.out.println(n/m);
+        System.out.println(n%m);
+    }
+}
