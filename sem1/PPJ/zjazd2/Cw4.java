cd23e235a931ec6f6487829ea9be2b89bc813b92
Oskar Korczak <oskarissimus@gmail.com>
2021-10-23 21:04:45 +0200

    cw4

---
+++ sem1/PPJ/zjazd2/Cw4.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main (String[] args) {
+        // 0x24229 - numer studenta w hexie
+        System.out.println(0b1001_0000_1000_1010_01); //numer studenta w hexie przekonwertowany na bin
+    }
+}
