96d04f2094bf767fafd12f1a2400158f91cf85a3
Oskar Korczak <oskar.korczak@protonmail.com>
2021-10-24 15:27:20 +0200

    fixing mistake in file naming

---
+++ sem1/PPJ/zjazd2/niedziela/Cw1.java
@@ -0,0 +1,8 @@
+public class Cw1 {
+
+    public static void main(String[] args) {
+        java.util.Scanner in = new java.util.Scanner(System.in);
+        int studentId = in.nextInt();
+        System.out.println("Hello s"+studentId);
+    }
+}
---
+++ sem1/PPJ/zjazd2/niedziela/Cw2.java
@@ -0,0 +1,9 @@
+public class Cw2 {
+    public static void main (String[] args) {
+        double a = 123 ;
+        double x ;
+        if ( a >= 0 ) x = 1.701 ;
+        else x = 2D*3.14f ;
+        System.out.println ( x ) ;
+    }
+}
