8d1d2e0a66130bc09fcc4d8042a53d82908c75a1
Oskar Korczak <oskar.korczak@protonmail.com>
2021-10-24 15:46:36 +0200

    cw 5

--- sem1/PPJ/zjazd1/Program2.java
+++ sem1/PPJ/zjazd1/Program2.java
@@ -1,5 +1,5 @@
 public class Program2 {
 	public static void main (String[] args) {
-		System.out.println('ladna pogoda');
+		System.out.println("ladna pogoda");
 	}
 }
---
+++ sem1/PPJ/zjazd2/niedziela/Cw5.java
@@ -0,0 +1,8 @@
+public class Cw5 {
+    public static void main(String[] args) {
+        int a=1, b=1, c=4;
+        String o = a == b ? b == c ? "sa takie same" : "nie sa takie same" : "nie sa takie same";
+        System.out.println(o);
+    }
+    
+}
