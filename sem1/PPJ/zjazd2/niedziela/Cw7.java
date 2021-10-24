43648cf6dc8e2d6a7f88dfa64d703b8f9b7731e9
Oskar Korczak <oskar.korczak@protonmail.com>
2021-10-24 16:10:46 +0200

    jedziemy

---
+++ sem1/PPJ/zjazd2/niedziela/Cw7.java
@@ -0,0 +1,8 @@
+public class Cw7 {
+    public static void main(String[] args){
+        double x = 13.37;
+        boolean x_is_in_A = x>=0;
+        boolean x_is_in_B = x<=1;
+        boolean x_is_in_C = x_is_in_A && x_is_in_B;
+    }
+}
