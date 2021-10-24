5a6a5393a219bf632d18bf98fc15676a89d00174
Oskar Korczak <oskar.korczak@protonmail.com>
2021-10-24 15:37:38 +0200

    cw 3 and 4

---
+++ sem1/PPJ/zjazd2/niedziela/Cw3.java
@@ -0,0 +1,13 @@
+public class Cw3 {
+    public static void main (String[] args) {
+        int zmInt = 4 ;
+        double zmDouble = -1.0;
+        if ( zmInt > 0 )
+            if ( zmDouble > 0 )
+                System.out.println ( "Here I am!" ) ;
+            else
+                System.out.println ( "No, I am here!" ) ;
+            System.out.println ( "No, actually , I am here!" ) ;
+    
+    }
+}
---
+++ sem1/PPJ/zjazd2/niedziela/Cw4.java
@@ -0,0 +1,8 @@
+public class Cw4 {
+    public static void main (String[] args) {
+        boolean makesBreakthrough = true;
+        boolean doesSignificantWork = true;
+        boolean nobelPrizeCandidate = doesSignificantWork && makesBreakthrough;
+        System.out.println(nobelPrizeCandidate);
+    }
+}
