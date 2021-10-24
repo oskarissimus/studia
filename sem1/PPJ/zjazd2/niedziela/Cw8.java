f99ecf4402dae2ec4462bc28ec9d7c32f3627c96
Oskar Korczak <oskar.korczak@protonmail.com>
2021-10-24 16:18:56 +0200

    cw8

---
+++ sem1/PPJ/zjazd2/niedziela/Cw8.java
@@ -0,0 +1,10 @@
+public class Cw8 {
+    public static void main(String[] args){
+        int wrt = 12;
+        if (wrt == -3 || wrt == -4) {
+            System.out.println("wrt nalezy do czesci wspolnej zbiorow A B C");
+        } else {
+            System.out.println("wrt nie nalezy do czesci wspolnej zbiorow A B C");
+        }
+    }
+}
