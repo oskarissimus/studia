441c0a4f31e723699255883b9d3cf2584b598fdc
Oskar Korczak <oskarissimus@gmail.com>
2021-11-07 15:27:02 +0100

    dwa pierwsze cw

---
+++ sem1/PPJ/zjazd3/niedziela/Cw1.java
@@ -0,0 +1,9 @@
+public class Cw1 {
+    public static void main(String[] args) {
+        int[] tab = new int[5];
+        for (int i = 0; i < 5; i++)
+            tab[i] = i * i;
+
+        System.out.println(tab[0]);
+    }
+}
---
+++ sem1/PPJ/zjazd3/niedziela/Cw2.java
@@ -0,0 +1,8 @@
+public class Cw2 {
+    public static void main(String[] args) {
+        int[] a = { 1, 2, 3 };
+        int[] b = { 1, 2, 3 };
+        System.out.println(a == b);
+        // flase - zawartosc obiektow taka sama ale obiekty inne
+    }
+}
