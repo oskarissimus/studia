0ccfec6d98b0948a31d2da3f787af596720d9bd7
Oskar Korczak <oskarissimus@gmail.com>
2021-12-04 20:22:34 +0100

    cw1 done

---
+++ sem1/PPJ/zjazd5/sobota/Cw1.java
@@ -0,0 +1,26 @@
+public class Cw1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int[] x = splitToDigits(87695);
+        for (int i = 0; i < x.length; i++) {
+            System.out.print(x[i]);
+        }
+        System.out.println("");
+    }
+
+    public static int[] splitToDigits(int number) {
+        int digitsNo = 0;
+        int num_left = number;
+        while (num_left > 0) {
+            digitsNo += 1;
+            num_left /= 10;
+        }
+        int[] digits = new int[digitsNo];
+        num_left = number;
+        for (int i = digitsNo - 1; i >= 0; i--) {
+            digits[i] = num_left % 10;
+            num_left /= 10;
+        }
+        return digits;
+    }
+}
