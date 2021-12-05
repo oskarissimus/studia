ad6e24d3977255d064eaac814030f3303f26db71
Oskar Korczak <oskarissimus@gmail.com>
2021-12-05 15:33:59 +0100

    palindrom from begin printing

---
+++ sem1/PPJ/zjazd5/sobota/Cw6.java
@@ -0,0 +1,33 @@
+public class Cw6 {
+    public static void main(String[] args) {
+        char[] s = { 'k', 'a', 'j', 'a', 'k' };
+        String formattedPalindrom = "";
+        if (isPalindrom(s)) {
+            for (int i = 0; i < s.length / 2; i++) {
+                formattedPalindrom = s[i] + formattedPalindrom + s[i];
+            }
+        }
+        System.out.println(formattedPalindrom);
+    }
+
+    public static boolean isPalindrom(char[] s) {
+        if (s.length == 1) {
+            return true;
+        } else if (s.length == 2) {
+            if (s[0] == s[1])
+                return true;
+            else
+                return false;
+        } else {
+            if (s[0] == s[s.length - 1]) {
+                char[] trimmed = new char[s.length - 2];
+                for (int i = 1; i < s.length - 1; i++) {
+                    trimmed[i - 1] = s[i];
+                }
+                return isPalindrom(trimmed);
+            } else
+                return false;
+
+        }
+    }
+}
